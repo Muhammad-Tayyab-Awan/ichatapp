8acfe1359922473d4c58ab1d94679a2251c47dd3
Muhammad-Tayyab-Awan <mtayyabawan1918@gmail.com>
2025-02-23 18:01:47 +0500

    Added Chat and NotFound routes to Router for improved navigation

--- Level-3/Project-6-Real-Time-Chat-Application/Frontend/src/router/Router.jsx
+++ Level-3/Project-6-Real-Time-Chat-Application/Frontend/src/router/Router.jsx
@@ -1,11 +1,15 @@
 import { Route, Routes } from "react-router";
 import Home from "../pages/Home";
+import Chat from "../pages/Chat";
+import NotFound from "../pages/NotFound";
 function Router() {
   return (
-      <Routes>
-          <Route path="/" element={<Home/>}/>
+    <Routes>
+      <Route path="/" element={<Home />} />
+      <Route path="/chat" element={<Chat />} />
+      <Route path="*" element={<NotFound />} />
     </Routes>
-  )
+  );
 }
 
 export default Router;
