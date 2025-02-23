1e931c2e8ff292bcc13f46c8d99f0b718fccb0d4
Muhammad-Tayyab-Awan <mtayyabawan1918@gmail.com>
2025-02-23 15:25:12 +0500

    Added React Router for navigation and create Home page

---
+++ Level-3/Project-6-Real-Time-Chat-Application/Frontend/src/router/Router.jsx
@@ -0,0 +1,11 @@
+import { Route, Routes } from "react-router";
+import Home from "../pages/Home";
+function Router() {
+  return (
+      <Routes>
+          <Route path="/" element={<Home/>}/>
+    </Routes>
+  )
+}
+
+export default Router;
