ec087991bfa3428fe76c3b60425880dc757d7bb7
Muhammad-Tayyab-Awan <mtayyabawan1918@gmail.com>
2025-02-23 18:02:16 +0500

    Refactored SimpleLayout to include Navbar and update background styles

--- Level-3/Project-6-Real-Time-Chat-Application/Frontend/src/layout/SimpleLayout.jsx
+++ Level-3/Project-6-Real-Time-Chat-Application/Frontend/src/layout/SimpleLayout.jsx
@@ -1,10 +1,11 @@
 /* eslint-disable react/prop-types */
+import Navbar from "../components/Navbar";
+
 function SimpleLayout({ children }) {
   return (
-    <div className="w-full bg-slate-950">
-      <div className="absolute top-[-10%] right-0 bottom-0 left-[-20%] h-[500px] w-[500px] rounded-full bg-[radial-gradient(circle_farthest-side,rgba(255,0,182,.15),rgba(255,255,255,0))]"></div>
-      <div className="min-h-screen text-amber-400">{children}</div>
-      <div className="absolute top-[-10%] bottom-0 h-[500px] w-[500px] rounded-full bg-[radial-gradient(circle_farthest-side,rgba(255,0,182,.15),rgba(255,255,255,0))]"></div>
+    <div className="h-full w-full bg-white">
+      <Navbar />
+      <div className="min-h-screen">{children}</div>
     </div>
   );
 }
