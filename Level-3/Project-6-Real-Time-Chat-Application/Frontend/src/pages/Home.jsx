6955446fa2247029c700898a254c379be9de74a7
Muhammad-Tayyab-Awan <mtayyabawan1918@gmail.com>
2025-02-24 21:46:43 +0500

    Added FeatureAccordion to Home page and updated layout for improved user engagement

--- Level-3/Project-6-Real-Time-Chat-Application/Frontend/src/pages/Home.jsx
+++ Level-3/Project-6-Real-Time-Chat-Application/Frontend/src/pages/Home.jsx
@@ -1,10 +1,12 @@
+import { Link } from "react-router";
+import FeatureAccordion from "../components/FeatureAccordian";
 import SimpleLayout from "../layout/SimpleLayout";
 
 function Home() {
   return (
     <SimpleLayout>
-      <div className="my-2 flex w-full flex-col items-center justify-around sm:flex-row">
-        <div className="flex h-full w-full flex-col items-center justify-center gap-2 sm:w-1/2 sm:gap-5">
+      <div className="my-2 flex w-full flex-col items-center justify-around md:flex-row">
+        <div className="flex h-full w-full flex-col items-center justify-center gap-2 md:w-1/2 md:gap-5">
           <h1 className="text-lg font-medium text-amber-600 sm:text-2xl">
             Real time Chat Application
           </h1>
@@ -16,7 +18,28 @@ function Home() {
             real-time text communication anytime, anywhere!
           </p>
         </div>
-        <img src="/chat-photo.jpg" alt="chat-photo" className="h-48 sm:h-96" />
+        <img
+          src="/images/chat-photo.jpg"
+          alt="chat-photo"
+          className="h-48 sm:h-96"
+        />
+      </div>
+      <div className="my-2 h-auto w-full">
+        <h2 className="text-center text-base font-medium sm:text-xl">
+          Features
+        </h2>
+        <div className="my-2 h-auto">
+          <FeatureAccordion />
+        </div>
+      </div>
+      <div className="mx-auto my-2 flex h-60 w-full flex-col items-center justify-center gap-3">
+        <h1 className="text-xl sm:text-2xl">Lets discover new friends</h1>
+        <Link
+          className="rounded-full bg-purple-400 p-2 text-sm font-medium text-white transition-all duration-300 ease-in-out hover:scale-105 sm:p-3 sm:text-base"
+          to="/chat"
+        >
+          Join Now
+        </Link>
       </div>
     </SimpleLayout>
   );
