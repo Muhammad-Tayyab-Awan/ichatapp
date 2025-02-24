cb268aacccc66cb123fc16366ef7676ad7932db2
Muhammad-Tayyab-Awan <mtayyabawan1918@gmail.com>
2025-02-24 16:55:35 +0500

    Added Footer component to SimpleLayout and adjusted main content height

--- Level-3/Project-6-Real-Time-Chat-Application/Frontend/src/layout/SimpleLayout.jsx
+++ Level-3/Project-6-Real-Time-Chat-Application/Frontend/src/layout/SimpleLayout.jsx
@@ -1,11 +1,13 @@
 /* eslint-disable react/prop-types */
+import Footer from "../components/Footer";
 import Navbar from "../components/Navbar";
 
 function SimpleLayout({ children }) {
   return (
     <div className="h-full w-full bg-white">
       <Navbar />
-      <div className="min-h-[calc(100vh-3.5rem)]">{children}</div>
+      <div className="min-h-[calc(100vh-7rem)]">{children}</div>
+      <Footer />
     </div>
   );
 }
