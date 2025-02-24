88b5863c944547da44c98663c055551e87ee0b68
Muhammad-Tayyab-Awan <mtayyabawan1918@gmail.com>
2025-02-24 19:02:46 +0500

    Refined Footer component layout and improved text sizing for better readability in the real-time chat application

--- Level-3/Project-6-Real-Time-Chat-Application/Frontend/src/components/Footer.jsx
+++ Level-3/Project-6-Real-Time-Chat-Application/Frontend/src/components/Footer.jsx
@@ -2,15 +2,15 @@ import { Link } from "react-router";
 
 function Footer() {
   return (
-    <footer className="flex h-14 w-full flex-col items-center justify-center bg-green-400">
+    <footer className="flex h-14 w-full flex-col items-center justify-around bg-green-400">
       <div className="flex w-full items-center justify-between px-4">
         <div className="flex items-center justify-center gap-2">
           <img src="/icon.svg" alt="logo" className="h-6 w-6" />
-          <p className="text-sm font-medium text-orange-600 sm:text-lg">
+          <p className="text-base font-medium text-orange-600 sm:text-lg">
             Chat Room
           </p>
         </div>
-        <div className="flex gap-4 text-xs sm:text-sm">
+        <div className="flex gap-4 text-sm sm:text-base">
           <Link
             className="transition-all duration-300 ease-in-out hover:underline"
             to="/privacy-policy"
@@ -25,7 +25,7 @@ function Footer() {
           </Link>
         </div>
       </div>
-      <p className="w-full text-center text-xs font-medium sm:text-sm">
+      <p className="w-full text-center text-xs sm:text-sm">
         &copy; {new Date().getFullYear()} | Chat Room | All rights reserved
       </p>
     </footer>
