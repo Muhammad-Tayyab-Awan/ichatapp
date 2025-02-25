e2107fa1c21003ceb356e722409a23e0a181d8e9
Muhammad-Tayyab-Awan <mtayyabawan1918@gmail.com>
2025-02-25 21:42:31 +0500

    Added Sidebar component to Chat layout for improved navigation

--- Level-3/Project-6-Real-Time-Chat-Application/Frontend/src/pages/Chat.jsx
+++ Level-3/Project-6-Real-Time-Chat-Application/Frontend/src/pages/Chat.jsx
@@ -1,7 +1,14 @@
+import Sidebar from "../components/Sidebar";
 import ChatLayout from "../layout/ChatLayout";
 
 function Chat() {
-  return <ChatLayout>Chat</ChatLayout>;
+  return (
+    <ChatLayout>
+      <div className="flex h-[calc(100vh-3.5rem)]">
+        <Sidebar />
+      </div>
+    </ChatLayout>
+  );
 }
 
 export default Chat;
