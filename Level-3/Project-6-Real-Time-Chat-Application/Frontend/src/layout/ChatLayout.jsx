c1ce28e34b8c6aaecf661c1aa1a5ff891d8759bf
Muhammad-Tayyab-Awan <mtayyabawan1918@gmail.com>
2025-02-25 19:45:52 +0500

    Added Toaster component for improved user notification display in ChatLayout

--- Level-3/Project-6-Real-Time-Chat-Application/Frontend/src/layout/ChatLayout.jsx
+++ Level-3/Project-6-Real-Time-Chat-Application/Frontend/src/layout/ChatLayout.jsx
@@ -1,9 +1,11 @@
+import { Toaster } from "react-hot-toast";
 import ChatHeader from "../components/ChatHeader";
 
 /* eslint-disable react/prop-types */
 function ChatLayout({ children }) {
   return (
     <div className="mx-auto h-full w-full max-w-[1536px] bg-white">
+      <Toaster />
       <ChatHeader />
       <div className="min-h-[calc(100vh-5rem)] sm:min-h-[calc(100vh-3.5rem)]">
         {children}
