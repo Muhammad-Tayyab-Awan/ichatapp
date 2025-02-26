7b895c01abd7ab0e0b702a6b0c7a207a22c53936
Muhammad-Tayyab-Awan <mtayyabawan1918@gmail.com>
2025-02-26 11:11:33 +0500

    Wrap Chat component with SidebarProvider for improved context management

--- Level-3/Project-6-Real-Time-Chat-Application/Frontend/src/pages/Chat.jsx
+++ Level-3/Project-6-Real-Time-Chat-Application/Frontend/src/pages/Chat.jsx
@@ -1,13 +1,16 @@
 import Sidebar from "../components/Sidebar";
+import { SidebarProvider } from "../context/SidebarContext";
 import ChatLayout from "../layout/ChatLayout";
 
 function Chat() {
   return (
-    <ChatLayout>
-      <div className="flex h-[calc(100vh-3.5rem)]">
-        <Sidebar />
-      </div>
-    </ChatLayout>
+    <SidebarProvider>
+      <ChatLayout>
+        <div className="flex h-[calc(100vh-3.5rem)]">
+          <Sidebar />
+        </div>
+      </ChatLayout>
+    </SidebarProvider>
   );
 }
 
