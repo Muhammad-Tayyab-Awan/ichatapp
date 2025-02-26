f812a822a845dedb0fe4c5e358908c9e770ca6b5
Muhammad-Tayyab-Awan <mtayyabawan1918@gmail.com>
2025-02-26 17:24:24 +0500

    Added layout state management to ChatHeader for dynamic button styling

--- Level-3/Project-6-Real-Time-Chat-Application/Frontend/src/components/ChatHeader.jsx
+++ Level-3/Project-6-Real-Time-Chat-Application/Frontend/src/components/ChatHeader.jsx
@@ -2,7 +2,7 @@ import { Link } from "react-router";
 import { useSidebarContext } from "../context/SidebarContext";
 
 function ChatHeader() {
-  const { setLayout } = useSidebarContext();
+  const { layout, setLayout } = useSidebarContext();
   return (
     <header className="sticky top-0 z-50 flex h-20 w-full flex-col items-center justify-evenly bg-green-400 sm:h-14 sm:flex-row sm:justify-between sm:px-2">
       <Link to="/" className="flex items-center justify-center gap-1 sm:gap-2">
@@ -13,7 +13,7 @@ function ChatHeader() {
       </Link>
       <div className="bg-white py-1 text-xs sm:bg-transparent sm:py-0 sm:text-sm sm:font-medium">
         <button
-          className="mx-1 cursor-pointer bg-purple-400 p-1 focus-visible:outline-hidden sm:rounded-sm sm:bg-white sm:p-2"
+          className={`mx-1 cursor-pointer bg-purple-400 p-1 focus-visible:outline-hidden sm:rounded-sm sm:bg-white sm:p-2 ${layout === "chats" && "ring"}`}
           onClick={() => {
             setLayout("chats");
           }}
@@ -21,7 +21,7 @@ function ChatHeader() {
           Chats
         </button>
         <button
-          className="mx-1 cursor-pointer bg-purple-400 p-1 focus-visible:outline-hidden sm:rounded-sm sm:bg-white sm:p-2"
+          className={`mx-1 cursor-pointer bg-purple-400 p-1 focus-visible:outline-hidden sm:rounded-sm sm:bg-white sm:p-2 ${layout === "onlineFriends" && "ring"}`}
           onClick={() => {
             setLayout("onlineFriends");
           }}
@@ -29,7 +29,7 @@ function ChatHeader() {
           Online Friends
         </button>
         <button
-          className="mx-1 cursor-pointer bg-purple-400 p-1 focus-visible:outline-hidden sm:rounded-sm sm:bg-white sm:p-2"
+          className={`mx-1 cursor-pointer bg-purple-400 p-1 focus-visible:outline-hidden sm:rounded-sm sm:bg-white sm:p-2 ${layout === "friends" && "ring"}`}
           onClick={() => {
             setLayout("friends");
           }}
@@ -37,7 +37,7 @@ function ChatHeader() {
           Friends
         </button>
         <button
-          className="mx-1 cursor-pointer bg-purple-400 p-1 focus-visible:outline-hidden sm:rounded-sm sm:bg-white sm:p-2"
+          className={`mx-1 cursor-pointer bg-purple-400 p-1 focus-visible:outline-hidden sm:rounded-sm sm:bg-white sm:p-2 ${layout === "yourProfile" && "ring"}`}
           onClick={() => {
             setLayout("yourProfile");
           }}
@@ -45,7 +45,7 @@ function ChatHeader() {
           Profile
         </button>
         <button
-          className="mx-1 cursor-pointer bg-purple-400 p-1 focus-visible:outline-hidden sm:rounded-sm sm:bg-white sm:p-2"
+          className={`mx-1 cursor-pointer bg-purple-400 p-1 focus-visible:outline-hidden sm:rounded-sm sm:bg-white sm:p-2 ${layout === "roomChat" && "ring"}`}
           onClick={() => {
             setLayout("roomChat");
           }}
