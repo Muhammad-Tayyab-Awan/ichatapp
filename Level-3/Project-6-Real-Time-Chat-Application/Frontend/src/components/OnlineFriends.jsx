4cd2b7abaf036b42e20d311d5847e7d7a708a0ab
Muhammad-Tayyab-Awan <mtayyabawan1918@gmail.com>
2025-02-26 12:06:11 +0500

    Added dynamic rendering of online friends using UserBar component

--- Level-3/Project-6-Real-Time-Chat-Application/Frontend/src/components/OnlineFriends.jsx
+++ Level-3/Project-6-Real-Time-Chat-Application/Frontend/src/components/OnlineFriends.jsx
@@ -1,5 +1,29 @@
+import UserBar from "./UserBar";
+
 function OnlineFriends() {
-  return <div>OnlineFriends</div>;
+  const friendsOnline = [
+    { username: "haiderworld", chatStatus: false, lastMsg: "none" },
+  ];
+  return (
+    <div>
+      <h1 className="text-center font-medium">Online Friends</h1>
+      {friendsOnline.length > 0 ? (
+        friendsOnline.map((friend) => {
+          return (
+            <UserBar
+              key={friend.username}
+              username={friend.username}
+              chatStatus={friend.chatStatus}
+            />
+          );
+        })
+      ) : (
+        <p className="my-3 text-center text-red-500">
+          None of your friend is online
+        </p>
+      )}
+    </div>
+  );
 }
 
 export default OnlineFriends;
