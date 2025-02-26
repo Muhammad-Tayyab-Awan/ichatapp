389ecee43a943e8f533ffc9225c35e8a282f59b6
Muhammad-Tayyab-Awan <mtayyabawan1918@gmail.com>
2025-02-26 12:01:31 +0500

    Added UserBar component to Friends for dynamic rendering of friend list

--- Level-3/Project-6-Real-Time-Chat-Application/Frontend/src/components/Friends.jsx
+++ Level-3/Project-6-Real-Time-Chat-Application/Frontend/src/components/Friends.jsx
@@ -1,5 +1,28 @@
+import UserBar from "./UserBar";
+
 function Friends() {
-  return <div>Friends</div>;
+  const friendList = [
+    { username: "hamza07", chatStatus: false, lastMsg: null },
+  ];
+  return (
+    <div>
+      <h1 className="text-center font-medium">Your Friends</h1>
+      {friendList.length > 0 ? (
+        friendList.map((friend) => {
+          return (
+            <UserBar
+              key={friend.username}
+              username={friend.username}
+              chatStatus={friend.chatStatus}
+              lastMsg={friend.lastMsg}
+            />
+          );
+        })
+      ) : (
+        <p className="my-3 text-center text-red-500">You have no friends yet</p>
+      )}
+    </div>
+  );
 }
 
 export default Friends;
