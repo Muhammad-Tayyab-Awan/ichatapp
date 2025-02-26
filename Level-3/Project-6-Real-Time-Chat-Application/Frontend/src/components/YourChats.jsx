26748527bb2f4f6fd3f2be2aca8134825e97ae7e
Muhammad-Tayyab-Awan <mtayyabawan1918@gmail.com>
2025-02-26 11:53:02 +0500

    Added YourChats component to display user chat list with dynamic rendering of UserBar for each chat

--- Level-3/Project-6-Real-Time-Chat-Application/Frontend/src/components/YourChats.jsx
+++ Level-3/Project-6-Real-Time-Chat-Application/Frontend/src/components/YourChats.jsx
@@ -1,5 +1,31 @@
+import UserBar from "./UserBar";
 function YourChats() {
-  return <div>YourChats</div>;
+  const userChats = [
+    { username: "tayyabraza1918", lastMsg: "Hi! this is me Tayyab Raza Awan" },
+    { username: "muneebattari34", lastMsg: "Hi! this is me Muneeb" },
+    { username: "kiyani47", lastMsg: "Hi! this is me Hassan" },
+  ];
+  return (
+    <div>
+      <h1 className="text-center font-medium">Your Chats</h1>
+      {userChats.length > 0 ? (
+        userChats.map((userChat) => {
+          return (
+            <UserBar
+              key={userChat.username}
+              username={userChat.username}
+              chatStatus={true}
+              lastMsg={userChat.lastMsg}
+            />
+          );
+        })
+      ) : (
+        <p className="my-3 text-center text-sm text-red-600">
+          Sorry! you have no chat with any friend
+        </p>
+      )}
+    </div>
+  );
 }
 
 export default YourChats;
