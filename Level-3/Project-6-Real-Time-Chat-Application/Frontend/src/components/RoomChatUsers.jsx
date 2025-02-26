d703ca42caaaf5c12536b7793ccb97743ad37643
Muhammad-Tayyab-Awan <mtayyabawan1918@gmail.com>
2025-02-26 12:11:36 +0500

    Added UserBar component to RoomChatUsers for dynamic rendering of users in the chat room

--- Level-3/Project-6-Real-Time-Chat-Application/Frontend/src/components/RoomChatUsers.jsx
+++ Level-3/Project-6-Real-Time-Chat-Application/Frontend/src/components/RoomChatUsers.jsx
@@ -1,5 +1,30 @@
+import UserBar from "./UserBar";
+
 function RoomChatUsers() {
-  return <div>RoomChatUsers</div>;
+  const roomUsers = [
+    { username: "tayyab", chatStatus: false },
+    { username: "raza", chatStatus: false },
+  ];
+  return (
+    <div>
+      <h1 className="text-center font-medium">Users in Room</h1>
+      {roomUsers.length > 0 ? (
+        roomUsers.map((user) => {
+          return (
+            <UserBar
+              key={user.username}
+              username={user.username}
+              chatStatus={user.chatStatus}
+            />
+          );
+        })
+      ) : (
+        <p className="my-3 text-center text-red-500">
+          No users are in the room now
+        </p>
+      )}
+    </div>
+  );
 }
 
 export default RoomChatUsers;
