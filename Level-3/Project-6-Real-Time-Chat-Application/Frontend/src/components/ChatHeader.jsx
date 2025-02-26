d6abba9b65a26e5e3ebd6484adae5d7e63de117c
Muhammad-Tayyab-Awan <mtayyabawan1918@gmail.com>
2025-02-26 11:09:50 +0500

    Updated ChatHeader component to integrate SidebarContext for dynamic layout changes

--- Level-3/Project-6-Real-Time-Chat-Application/Frontend/src/components/ChatHeader.jsx
+++ Level-3/Project-6-Real-Time-Chat-Application/Frontend/src/components/ChatHeader.jsx
@@ -1,6 +1,8 @@
 import { Link } from "react-router";
+import { useSidebarContext } from "../context/SidebarContext";
 
 function ChatHeader() {
+  const { setLayout } = useSidebarContext();
   return (
     <header className="sticky top-0 z-50 flex h-20 w-full flex-col items-center justify-evenly bg-green-400 sm:h-14 sm:flex-row sm:justify-between sm:px-2">
       <Link to="/" className="flex items-center justify-center gap-1 sm:gap-2">
@@ -10,20 +12,45 @@ function ChatHeader() {
         </p>
       </Link>
       <div className="bg-white py-1 text-xs sm:bg-transparent sm:py-0 sm:text-sm sm:font-medium">
-        <button className="mx-1 cursor-pointer bg-purple-400 p-1 focus-visible:outline-hidden sm:rounded-sm sm:bg-white sm:p-2">
+        <button
+          className="mx-1 cursor-pointer bg-purple-400 p-1 focus-visible:outline-hidden sm:rounded-sm sm:bg-white sm:p-2"
+          onClick={() => {
+            setLayout("chats");
+          }}
+        >
           Chats
         </button>
-        <button className="mx-1 cursor-pointer bg-purple-400 p-1 focus-visible:outline-hidden sm:rounded-sm sm:bg-white sm:p-2">
-          Online Users
+        <button
+          className="mx-1 cursor-pointer bg-purple-400 p-1 focus-visible:outline-hidden sm:rounded-sm sm:bg-white sm:p-2"
+          onClick={() => {
+            setLayout("onlineFriends");
+          }}
+        >
+          Online Friends
         </button>
-        <button className="mx-1 cursor-pointer bg-purple-400 p-1 focus-visible:outline-hidden sm:rounded-sm sm:bg-white sm:p-2">
-          All Users
+        <button
+          className="mx-1 cursor-pointer bg-purple-400 p-1 focus-visible:outline-hidden sm:rounded-sm sm:bg-white sm:p-2"
+          onClick={() => {
+            setLayout("friends");
+          }}
+        >
+          Friends
         </button>
-        <button className="mx-1 cursor-pointer bg-purple-400 p-1 focus-visible:outline-hidden sm:rounded-sm sm:bg-white sm:p-2">
+        <button
+          className="mx-1 cursor-pointer bg-purple-400 p-1 focus-visible:outline-hidden sm:rounded-sm sm:bg-white sm:p-2"
+          onClick={() => {
+            setLayout("yourProfile");
+          }}
+        >
           Profile
         </button>
-        <button className="mx-1 cursor-pointer bg-purple-400 p-1 focus-visible:outline-hidden sm:rounded-sm sm:bg-white sm:p-2">
-          Go Offline
+        <button
+          className="mx-1 cursor-pointer bg-purple-400 p-1 focus-visible:outline-hidden sm:rounded-sm sm:bg-white sm:p-2"
+          onClick={() => {
+            setLayout("roomChat");
+          }}
+        >
+          Room Chat
         </button>
       </div>
     </header>
