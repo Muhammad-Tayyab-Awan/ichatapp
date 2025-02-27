a8818e5d25cedc2c3f697e6c17cbb9b5767669a5
Muhammad-Tayyab-Awan <mtayyabawan1918@gmail.com>
2025-02-27 16:54:06 +0500

    Refactored Chat component to include ChatDisplay and wrap with ChatHeaderProvider for improved layout management

--- Level-3/Project-6-Real-Time-Chat-Application/Frontend/src/pages/Chat.jsx
+++ Level-3/Project-6-Real-Time-Chat-Application/Frontend/src/pages/Chat.jsx
@@ -1,18 +1,23 @@
+import ChatDisplay from "../components/ChatDisplay";
 import SearchBar from "../components/SearchBar";
 import Sidebar from "../components/Sidebar";
+import { ChatHeaderProvider } from "../context/ChatHeaderContext";
 import { SidebarProvider } from "../context/SidebarContext";
 import ChatLayout from "../layout/ChatLayout";
 
 function Chat() {
   return (
-    <SidebarProvider>
-      <ChatLayout>
-        <div className="relative flex h-[calc(100vh-3.5rem)]">
-          <Sidebar />
-          <SearchBar />
-        </div>
-      </ChatLayout>
-    </SidebarProvider>
+    <ChatHeaderProvider>
+      <SidebarProvider>
+        <ChatLayout>
+          <div className="relative flex h-[calc(100vh-3.5rem)]">
+            <Sidebar />
+            <ChatDisplay />
+            <SearchBar />
+          </div>
+        </ChatLayout>
+      </SidebarProvider>
+    </ChatHeaderProvider>
   );
 }
 
