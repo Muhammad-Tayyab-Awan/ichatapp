5348d3045eed04c150c9aa6f46269902f2bc7963
Muhammad-Tayyab-Awan <mtayyabawan1918@gmail.com>
2025-02-28 12:16:48 +0500

    Added Auth route to Router for user authentication page

--- Level-3/Project-6-Real-Time-Chat-Application/Frontend/src/router/Router.jsx
+++ Level-3/Project-6-Real-Time-Chat-Application/Frontend/src/router/Router.jsx
@@ -4,13 +4,15 @@ import Chat from "../pages/Chat";
 import NotFound from "../pages/NotFound";
 import About from "../pages/About";
 import PrivacyPolicy from "../pages/PrivacyPolicy";
+import Auth from "../pages/Auth";
 function Router() {
   return (
     <Routes>
-      <Route path="/" element={<Home />} />
+      <Route index element={<Home />} />
       <Route path="/chat" element={<Chat />} />
       <Route path="/about-us" element={<About />} />
       <Route path="/privacy-policy" element={<PrivacyPolicy />} />
+      <Route path="/auth" element={<Auth />} />
       <Route path="*" element={<NotFound />} />
     </Routes>
   );
