334383a3c0b0312ccb8bd7931007a3549ae571f9
Muhammad-Tayyab-Awan <mtayyabawan1918@gmail.com>
2025-02-28 10:37:02 +0500

    Added DefaultMsgBox component for welcome message display

---
+++ Level-3/Project-6-Real-Time-Chat-Application/Frontend/src/components/DefaultMsgBox.jsx
@@ -0,0 +1,11 @@
+function DefaultMsgBox() {
+  return (
+    <div className="flex h-full w-full flex-col items-center justify-start py-20">
+      <img src="/icon.svg" alt="icon" />
+      <h1 className="text-xl font-medium">Welcome to IChat</h1>
+      <p>Enjoy chatting with your friends</p>
+    </div>
+  );
+}
+
+export default DefaultMsgBox;
