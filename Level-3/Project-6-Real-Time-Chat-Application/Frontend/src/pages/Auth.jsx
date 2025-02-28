97bf0b23a80eebed87e1ab03451220b36b5784ed
Muhammad-Tayyab-Awan <mtayyabawan1918@gmail.com>
2025-02-28 18:37:25 +0500

    Enhanced Auth component to support OTP verification alongside Register and Login forms

--- Level-3/Project-6-Real-Time-Chat-Application/Frontend/src/pages/Auth.jsx
+++ Level-3/Project-6-Real-Time-Chat-Application/Frontend/src/pages/Auth.jsx
@@ -1,16 +1,15 @@
 import { useState } from "react";
 import Register from "../components/Register";
 import Login from "../components/Login";
+import OTPVerification from "../components/OTPVerification";
 
 function Auth() {
   const [form, setForm] = useState("register");
   return (
     <div className="flex h-screen w-full items-center justify-center bg-amber-500">
-      {form === "register" ? (
-        <Register setForm={setForm} />
-      ) : (
-        <Login setForm={setForm} />
-      )}
+      {(form === "register" && <Register setForm={setForm} />) ||
+        (form === "login" && <Login setForm={setForm} />) ||
+        (form === "otp" && <OTPVerification />)}
     </div>
   );
 }
