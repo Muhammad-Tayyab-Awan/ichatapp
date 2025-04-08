aa3307f2e6dbd730b44dba92bd249b3a4690f917
Muhammad-Tayyab-Awan <mtayyabawan1918@gmail.com>
2025-04-08 14:59:34 +0500

    added middleware to verify user is logged in or not

---
+++ Backend/middleware/verifyLogin.js
@@ -0,0 +1,37 @@
+/* eslint-disable no-undef */
+import jwt from "jsonwebtoken";
+import User from "../models/Users.js";
+const jwtSecret = process.env.JWT_SECRET;
+const verifyLogin = async (req, res, next) => {
+  try {
+    const authToken = req.cookies["ichat_auth_token"];
+    console.log(authToken);
+    jwt.verify(authToken, jwtSecret, async (err, decodedToken) => {
+      if (err) {
+        return res.status(404).json({
+          success: false,
+          error: "Invalid request",
+          message: "Not a valid token",
+        });
+      }
+      const userId = decodedToken.userId;
+      const userExist = await User.findById(userId);
+      if (!(userExist && userExist.verified)) {
+        return res.status(404).json({
+          success: false,
+          error: "Invalid request",
+          message: "Not a valid token",
+        });
+      }
+      req.userId = userId;
+      next();
+    });
+  } catch (error) {
+    res.status(500).json({
+      success: false,
+      error: "Error Occurred on Server Side",
+      message: error.message,
+    });
+  }
+};
+export default verifyLogin;
