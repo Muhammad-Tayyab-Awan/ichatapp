05e498dccb0e10fe7cd98cc6a65d417ed41bdb93
Muhammad-Tayyab-Awan <mtayyabawan1918@gmail.com>
2025-04-08 16:18:19 +0500

    added blocked field in modal to store ids of blocked users

--- Backend/models/Users.js
+++ Backend/models/Users.js
@@ -1,58 +1,68 @@
 import mongoose, { Schema } from "mongoose";
 const friendsSchema = new Schema(
   {
-    userId: { type: Schema.Types.ObjectId, required: true, ref: "user" }
+    userId: { type: Schema.Types.ObjectId, required: true, ref: "user" },
   },
-  { timestamps: true }
+  { timestamps: true },
+);
+const blockedSchema = new Schema(
+  {
+    userId: { type: Schema.Types.ObjectId, required: true, ref: "user" },
+  },
+  { timestamps: true },
 );
 const userSchema = new Schema(
   {
     username: {
       type: String,
       unique: true,
-      required: true
+      required: true,
     },
     firstName: {
       type: String,
-      required: true
+      required: true,
     },
     lastName: {
       type: String,
-      required: true
+      required: true,
     },
     email: {
       type: String,
       unique: true,
-      required: true
+      required: true,
     },
     birthdate: {
       type: String,
-      required: true
+      required: true,
     },
     gender: {
       type: String,
       required: true,
-      enum: ["male", "female"]
+      enum: ["male", "female"],
     },
     password: {
       type: String,
-      required: true
+      required: true,
     },
     verified: {
       type: Boolean,
-      default: false
+      default: false,
     },
     status: {
       type: String,
       enum: ["blocked", "simple"],
-      default: "simple"
+      default: "simple",
     },
     friends: {
       type: [friendsSchema],
-      default: []
-    }
+      default: [],
+    },
+    blocked: {
+      type: [blockedSchema],
+      default: [],
+    },
   },
-  { timestamps: true }
+  { timestamps: true },
 );
 const User = mongoose.model("user", userSchema);
 export default User;
