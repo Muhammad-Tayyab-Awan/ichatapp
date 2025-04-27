ba2524ff8909c98a7e9fbaad608803a4a465ae82
Muhammad-Tayyab-Awan <mtayyabawan1918@gmail.com>
2025-04-27 20:07:55 +0500

    added useAuth hook for authorization

---
+++ src/hooks/useAuth.jsx
@@ -0,0 +1,25 @@
+import { useState } from "react";
+import { useEffect } from "react";
+import authAPI from "../api/authApi";
+
+const useAuth = () => {
+  const [isLoggedIn, setIsLoggedIn] = useState({
+    loggedIn: null,
+    userId: null,
+  });
+  useEffect(() => {
+    authAPI.loginStatus().then((response) => {
+      setIsLoggedIn(response);
+    });
+  }, []);
+  if (isLoggedIn.loggedIn === null) {
+    return { isLoading: true };
+  }
+  if (isLoggedIn.loggedIn === false) {
+    return { isLoggedIn: false };
+  } else {
+    return { isLoggedIn: true, userId: isLoggedIn.userId };
+  }
+};
+
+export default useAuth;
