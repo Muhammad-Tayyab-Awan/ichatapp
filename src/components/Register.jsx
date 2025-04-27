2766b620aa34c7f8bc980c28f6e7595c92a33ea9
Muhammad-Tayyab-Awan <mtayyabawan1918@gmail.com>
2025-04-27 18:52:54 +0500

    added registration functionality to register component

--- src/components/Register.jsx
+++ src/components/Register.jsx
@@ -1,8 +1,78 @@
 /* eslint-disable react/prop-types */
+import { joiResolver } from "@hookform/resolvers/joi";
+import Joi from "joi";
 import { useForm } from "react-hook-form";
+import authAPI from "../api/authAPI";
+import toast from "react-hot-toast";
+
+const schema = Joi.object({
+  username: Joi.string()
+    .pattern(/^[a-z0-9]{6,15}$/)
+    .required()
+    .messages({
+      "string.pattern.base":
+        "Username must consist of 6 to 15 chars (lowercase and numbers only)",
+      "string.empty": "Username is required",
+    }),
+
+  email: Joi.string()
+    .email({ tlds: { allow: false } })
+    .required()
+    .messages({
+      "string.empty": "Email is required",
+      "string.email": "Please enter a valid email",
+    }),
+
+  password: Joi.string()
+    .min(8)
+    .pattern(/[a-z].*[a-z].*[a-z]/, "at least 3 lowercase letters")
+    .pattern(/[A-Z].*[A-Z]/, "at least 2 uppercase letters")
+    .pattern(/\d.*\d/, "at least 2 numbers")
+    .pattern(/[^a-zA-Z0-9]/, "at least 1 symbols")
+    .required()
+    .messages({
+      "string.empty": "Password is required",
+      "string.min": "Password must be at least 8 characters long.",
+      "string.pattern.name":
+        "Password must contain at least 2 uppercase, 3 lowercase, 2 numbers, and 1 symbols.",
+    }),
+
+  firstName: Joi.string()
+    .pattern(/^[A-Z][a-z]{3,20}$/)
+    .required()
+    .messages({
+      "string.pattern.base":
+        "First name must start with an uppercase letter and contain 3 to 20 characters",
+      "string.empty": "First name is required",
+    }),
+
+  lastName: Joi.string()
+    .pattern(/^[A-Z][a-z]{3,30}$/)
+    .required()
+    .messages({
+      "string.pattern.base":
+        "Last name must start with an uppercase letter and contain 3 to 30 characters",
+      "string.empty": "Last name is required",
+    }),
+
+  gender: Joi.string().valid("male", "female").required().messages({
+    "any.only": "Gender must be either 'male' or 'female'",
+    "string.empty": "Gender is required",
+  }),
+
+  birthdate: Joi.date().required().messages({
+    "date.empty": "Address is required",
+  }),
+});
 
 function Register({ setForm }) {
-  const { handleSubmit, register } = useForm({
+  const {
+    handleSubmit,
+    register,
+    reset,
+    formState: { errors, isSubmitting },
+  } = useForm({
+    resolver: joiResolver(schema),
     defaultValues: {
       username: "",
       firstName: "",
@@ -13,16 +83,22 @@ function Register({ setForm }) {
       birthdate: "",
     },
   });
+  const submitForm = async (data) => {
+    const response = await authAPI.register(data);
+    reset();
+    if (response.success === "otp") {
+      setForm("otp");
+      return;
+    }
+    toast.error(response.error);
+  };
   return (
     <form
       className="flex h-96 w-96 flex-col items-center justify-center gap-4 rounded-lg bg-gray-100/10 p-2 shadow-lg shadow-black backdrop-blur-xs"
       noValidate
       autoComplete="off"
       method="POST"
-      onSubmit={handleSubmit((data) => {
-        console.log(data);
-        setForm("otp");
-      })}
+      onSubmit={handleSubmit(submitForm)}
     >
       <h1 className="text-center font-medium">
         Register Now
@@ -46,7 +122,7 @@ function Register({ setForm }) {
           {...register("firstName", { required: true })}
           autoComplete="off"
           placeholder="First name here"
-          className="w-[75%] rounded-sm bg-white px-2 py-1 text-center text-xs text-black shadow-sm shadow-black transition-shadow duration-200 ease-in-out placeholder:text-gray-950 focus-visible:shadow-md focus-visible:outline-hidden"
+          className={`w-[75%] rounded-sm bg-white px-2 py-1 text-center text-xs text-black shadow-sm shadow-black transition-shadow duration-200 ease-in-out placeholder:text-gray-950 focus-visible:shadow-md focus-visible:outline-hidden ${errors.firstName && "ring-2 ring-red-800"}`}
         />
       </div>
       <div className="flex w-full items-center justify-center">
@@ -57,7 +133,7 @@ function Register({ setForm }) {
           {...register("lastName", { required: true })}
           autoComplete="off"
           placeholder="Last name here"
-          className="w-[75%] rounded-sm bg-white px-2 py-1 text-center text-xs text-black shadow-sm shadow-black transition-shadow duration-200 ease-in-out placeholder:text-gray-950 focus-visible:shadow-md focus-visible:outline-hidden"
+          className={`w-[75%] rounded-sm bg-white px-2 py-1 text-center text-xs text-black shadow-sm shadow-black transition-shadow duration-200 ease-in-out placeholder:text-gray-950 focus-visible:shadow-md focus-visible:outline-hidden ${errors.lastName && "ring-2 ring-red-800"}`}
         />
       </div>
       <div className="flex w-full items-center justify-center">
@@ -68,7 +144,7 @@ function Register({ setForm }) {
           autoComplete="off"
           {...register("username", { required: true })}
           placeholder="Username here"
-          className="w-[75%] rounded-sm bg-white px-2 py-1 text-center text-xs text-black shadow-sm shadow-black transition-shadow duration-200 ease-in-out placeholder:text-gray-950 focus-visible:shadow-md focus-visible:outline-hidden"
+          className={`w-[75%] rounded-sm bg-white px-2 py-1 text-center text-xs text-black shadow-sm shadow-black transition-shadow duration-200 ease-in-out placeholder:text-gray-950 focus-visible:shadow-md focus-visible:outline-hidden ${errors.username && "ring-2 ring-red-800"}`}
         />
       </div>
       <div className="flex w-full items-center justify-center">
@@ -79,7 +155,7 @@ function Register({ setForm }) {
           {...register("email", { required: true })}
           autoComplete="off"
           placeholder="Email here"
-          className="w-[75%] rounded-sm bg-white px-2 py-1 text-center text-xs text-black shadow-sm shadow-black transition-shadow duration-200 ease-in-out placeholder:text-gray-950 focus-visible:shadow-md focus-visible:outline-hidden"
+          className={`w-[75%] rounded-sm bg-white px-2 py-1 text-center text-xs text-black shadow-sm shadow-black transition-shadow duration-200 ease-in-out placeholder:text-gray-950 focus-visible:shadow-md focus-visible:outline-hidden ${errors.email && "ring-2 ring-red-800"}`}
         />
       </div>
       <div className="flex w-full items-center justify-center">
@@ -90,7 +166,7 @@ function Register({ setForm }) {
           autoComplete="off"
           {...register("password", { required: true })}
           placeholder="Password here"
-          className="w-[75%] rounded-sm bg-white px-2 py-1 text-center text-xs text-black shadow-sm shadow-black transition-shadow duration-200 ease-in-out placeholder:text-gray-950 focus-visible:shadow-md focus-visible:outline-hidden"
+          className={`w-[75%] rounded-sm bg-white px-2 py-1 text-center text-xs text-black shadow-sm shadow-black transition-shadow duration-200 ease-in-out placeholder:text-gray-950 focus-visible:shadow-md focus-visible:outline-hidden ${errors.password && "ring-2 ring-red-800"}`}
         />
       </div>
       <div className="flex w-full items-center justify-center text-xs">
@@ -99,24 +175,39 @@ function Register({ setForm }) {
           name="birthdate"
           id="birthdate"
           autoComplete="off"
-          {...register("birthdate", { required: true })}
-          className="w-[75%] rounded-sm bg-white px-2 py-1 text-center text-xs text-black shadow-sm shadow-black transition-shadow duration-200 ease-in-out placeholder:text-gray-950 focus-visible:shadow-md focus-visible:outline-hidden"
+          {...register("birthdate", { required: true, valueAsDate: true })}
+          className={`w-[75%] rounded-sm bg-white px-2 py-1 text-center text-xs text-black shadow-sm shadow-black transition-shadow duration-200 ease-in-out placeholder:text-gray-950 focus-visible:shadow-md focus-visible:outline-hidden ${errors.birthdate && "ring-2 ring-red-800"}`}
         />
       </div>
       <div className="flex w-full items-center justify-center gap-3 text-xs">
         <span className="font-medium">Gender :</span>
-        <input type="radio" name="gender" id="male" value="male" />
+        <input
+          type="radio"
+          name="gender"
+          {...register("gender")}
+          id="male"
+          value="male"
+          className={errors.gender && "ring-2 ring-red-800"}
+        />
         <label htmlFor="male">Male</label>
-        <input type="radio" name="gender" id="female" value="female" />
+        <input
+          type="radio"
+          name="gender"
+          {...register("gender")}
+          id="female"
+          value="female"
+          className={errors.gender && "ring-2 ring-red-800"}
+        />
         <label htmlFor="female">Female</label>
       </div>
       <div>
-        <input
+        <button
           type="submit"
           name="submit"
-          value="Register"
-          className="cursor-pointer rounded-sm bg-white p-1 text-xs font-medium text-black shadow-sm shadow-black focus-visible:outline-hidden"
-        />
+          className={`cursor-pointer rounded-sm bg-white p-1 text-xs font-medium text-black shadow-sm shadow-black focus-visible:outline-hidden ${isSubmitting && "animate-pulse"}`}
+        >
+          {isSubmitting ? "Loading..." : "Register"}
+        </button>
       </div>
     </form>
   );
